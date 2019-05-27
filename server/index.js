bd29227fe091686aa2b3606f6dbd9d71ddd6fd81
Jesse Lessinger <jesse.lessinger@gmail.com>
2019-05-27 16:06:31 -0500

    adds bodyParser middleware and root ('/') endpoint

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  console.log(req.method, req.url)
+});
 
 const PORT = process.env.port || 8080
 
