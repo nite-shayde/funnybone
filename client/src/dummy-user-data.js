bdc93ef8707775ce3d6a1b0a02c82219539f0dcb
Jesse Lessinger <jesse.lessinger@gmail.com>
2019-05-28 16:45:58 -0500

    Add basic structure for app component layout, w dummy user data

---
+++ client/src/dummy-user-data.js
@@ -0,0 +1,15 @@
+module.exports = {
+  joe: {
+    username: "joeyjojo",
+    name: "Joey Jo Jo",
+    email: "joey@jojo.com",
+    interests: [
+      'Dave Chappelle',
+      'Tiffany Haddish',
+      'The Office',
+      'You can leave now'
+    ],
+    profilePicURL: 'https://i.cdn.turner.com/adultswim/big/img/2018/01/09/RickMorty2_Marathon_2.jpg'
+
+  }
+}
