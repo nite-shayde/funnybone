10c17e961dc051454265cac3f0c6aeaa35494786
Chris Corley <chris.corley05@gmail.com>
2019-05-29 01:44:18 -0500

    Add button to each interest list item, and resizes thumbnail

--- client/src/components/user-profile.jsx
+++ client/src/components/user-profile.jsx
@@ -12,10 +12,10 @@ function Proflie(props) {
     // console.log(props.user.interests);
     return (
         <div>
-            <img src={props.user.profilePicURL} />
-            <h1>@{props.user.username}</h1>
-            <h2>{props.user.name}</h2>
-            <ul>{props.user.interests.map(interest => <li>{interest}</li>)}</ul>
+            <img class="img-thumbnail" src={props.user.profilePicURL} />
+            <h2><strong><small>@ </small>{props.user.username}</strong></h2>
+            <h3><i>{props.user.name}</i></h3>
+            <ul class="list-group">{props.user.interests.map(interest => <button type="button" class="list-group-item list-group-item-action">{interest}</button>)}</ul>
         </div>
     );
 }
