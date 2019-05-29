fde4704577f406e28c295adbe42131eff3f4005c
kalkidan <kalkidanmulatu2@gmail.com>
2019-05-29 13:10:13 -0500

    This will set up database connection

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -1,1 +1,33 @@
 // this is where the database connection will happen 
+const Sequelize = require('sequelize');
+
+const host = 'localhost';
+const user = 'root';
+const port = 3306;
+const password = '';
+const database = 'joke-me-off';
+
+const connection = new Sequelize(database, user, password, {
+  dialect: 'mysql',
+  host,
+  port,
+});
+
+const Users = connection.define('users', {
+    name: Sequelize.STRING,
+    user_name: Sequelize.STRING,
+    profile_pic: Sequelize.STRING.BINARY
+})
+const Messages = connection.define('messages', {
+    text: Sequelize.STRING,
+    from_id: Sequelize.INTEGER,
+    to_id: Sequelize.INTEGER
+})
+
+connection.sync()
+  .then((result) => {
+    console.log(result, 'are we in the data??????');
+  }).catch((err) => {
+    console.log(err, '!!!!!!!!!!!!!');
+  });
+
