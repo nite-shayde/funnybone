c59b89963bbc03b65ff8b68b51a625dc47f2124a
Chris Corley <chris.corley05@gmail.com>
2019-05-29 10:13:54 -0500

    Removes style tags from user-profile.jsx

--- client/src/components/user-profile.jsx
+++ client/src/components/user-profile.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
 
-// const InterestList = props => {
-//     <ol>
-//         <li>{props.user.interest}</li>
-//     </ol>
-//     console.log(props.user.interest);
-// }
-
 // props is the user data
 function Proflie(props) {
     // console.log(props.user.interests);
     return (
         <div>
-            <img class="img-thumbnail" src={props.user.profilePicURL} />
-            <h2><strong><small>@ </small>{props.user.username}</strong></h2>
-            <h3><i>{props.user.name}</i></h3>
-            <ul class="list-group">{props.user.interests.map(interest => <button type="button" class="list-group-item list-group-item-action">{interest}</button>)}</ul>
+            <img className="img-thumbnail" src={props.user.profilePicURL} />
+            <h2>@{props.user.username}</h2>
+            <h3>{props.user.name}</h3>
+            <ul className="list-group">{props.user.interests.map(interest => <button type="button" className="list-group-item list-group-item-action">{interest}</button>)}</ul>
         </div>
     );
 }
