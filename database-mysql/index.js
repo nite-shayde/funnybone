f5d8c14900c11c9ae48be58337bb57b2d4ba4f78
kalkidan <kalkidanmulatu2@gmail.com>
2019-05-29 18:44:33 -0500

    populate the USERS table with some dummy data

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -32,6 +32,23 @@ connection.sync()
     console.log(err, '!!!!!!!!!!!!!');
   });
 
+  Users.create({
+    name: 'Kalkidan',
+    userName: 'KalkidanMulatu',
+    profilePic: 'https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-0/p110x80/48359292_220566528875309_249279966284349440_n.jpg?_nc_cat=104&_nc_ht=scontent-dfw5-1.xx&oh=c87c3905e336d7235594112d4b2d3db0&oe=5D91B150' 
+  })
+  Users.create({
+    name: 'Kelly',
+    userName: 'KellyT',
+    profilePic: 'https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-0/p110x80/48359292_220566528875309_249279966284349440_n.jpg?_nc_cat=104&_nc_ht=scontent-dfw5-1.xx&oh=c87c3905e336d7235594112d4b2d3db0&oe=5D91B150' 
+  })
+  Users.create({
+    name: 'Ezra',
+    userName: 'EzraMoges',
+    profilePic: 'https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-0/p110x80/48359292_220566528875309_249279966284349440_n.jpg?_nc_cat=104&_nc_ht=scontent-dfw5-1.xx&oh=c87c3905e336d7235594112d4b2d3db0&oe=5D91B150' 
+  })
+
+
   module.exports.connection = connection;
   module.exports.Messages = Messages;
   module.exports.Users = Users;
