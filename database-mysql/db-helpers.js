3a58f22ccf0fdfda786968ba19ea989a677b66ec
kalkidan <kalkidanmulatu2@gmail.com>
2019-05-30 16:25:48 -0500

    saveUser to the DB function and saveMessage function

--- database-mysql/db-helpers.js
+++ database-mysql/db-helpers.js
@@ -1,5 +1,5 @@
 const { Users } = require('./index');
-const Messages = require('./index');
+const { Messages } = require('./index');
 
 const getUsers = () => {
  Users.findAll()
@@ -11,6 +11,42 @@ const getUsers = () => {
     })
   };
 
-   getUsers();
+  // getUsers();
+ const getConversation = (userAid, userBid) => {
+
+ }
+
+// db findAll :
+
+// WHERE
+// fromUserId === userAid AND toUserId === userBid
+// OR
+// fromUserId === userBid AND toUserId === userAid
+
+// SORT BY:
+// createAt (most recent first)
+const saveUser = (user) => {
+    Users.create(user)
+    .then((user) => {
+        console.log(user)
+    }).catch((err) => {
+        console.log(err)
+    });
+}
+
+//saveUser({name: "Chris", username: "chrisCorley", email: "chrisCorly@gmail.com", profilePicURL: "pizzahut.com"})
+const saveMessage = (message) => {
+    Messages.create(message)
+    .then((message) => {
+        console.log(message)
+    }).catch((err) => {
+        console.log(err)
+    });
+
+}
+saveMessage({contentType: "text",
+    content: "I Don't think Chris is funny",
+    fromId: 3,
+    toId: 1})
   module.exports.getUsers = getUsers;
   
