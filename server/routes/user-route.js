2b58c94ee24fbecd33247c14bee2aa2717ff9e81
Chris Corley <chris.corley05@gmail.com>
2019-05-30 17:00:06 -0500

    Adds get request for user-route

--- server/routes/user-route.js
+++ server/routes/user-route.js
@@ -1,13 +1,21 @@
 const express = require('express');
-
+const helpers = require('../../database-mysql/db-helpers');
+// const bodyParser = require('body-parser');
 const router = express.Router();
+// app.use(bodyParser.json());
 
-router.get('/', function(req, res) {
-    res.send('GET handler for /api/user route.');
+router.get('/', function (req, res) {
+    helpers.getUsers()
+        .then((users) => {
+            res.send(users)
+        }).catch((err) => {
+            res.send(err)
+        })
+    // res.send('GET handler for /api/user route.').status(200);
 });
 
-router.post('/', function(req, res) {
-    res.send('POST handler for /api/user route.');
+router.post('/', function (req, res) {
+    // res.send('POST handler for /api/user route.');
 });
 
 module.exports = router;
