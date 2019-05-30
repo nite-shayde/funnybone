6f218d7017d82f7ac824c42f62a2064c7b6787fd
kalkidan <kalkidanmulatu2@gmail.com>
2019-05-30 10:55:31 -0500

    added an email to my dummy data

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -1,5 +1,6 @@
 // this is where the database connection will happen 
 const Sequelize = require('sequelize');
+const dummyUserData = require('../client/src/dummy-user-data');
 
 const host = 'localhost';
 const user = process.env.DB_USER || 'root';
@@ -23,7 +24,7 @@ const Users = connection.define('users', {
       unique: true,
       allowNull: false,
     },
-    profilePic: Sequelize.STRING 
+    profilePicURL: Sequelize.STRING 
 })
 const Messages = connection.define('messages', {
   contentType: Sequelize.STRING,
@@ -44,17 +45,20 @@ connection.sync({ force: false })
   {
     name: 'Kalkidan',
     userName: 'KalkidanMulatu',
-    profilePic: 'https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-0/p110x80/48359292_220566528875309_249279966284349440_n.jpg?_nc_cat=104&_nc_ht=scontent-dfw5-1.xx&oh=c87c3905e336d7235594112d4b2d3db0&oe=5D91B150' 
+    email: 'kalki@gmail.com',
+    profilePicURL: 'https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-0/p110x80/48359292_220566528875309_249279966284349440_n.jpg?_nc_cat=104&_nc_ht=scontent-dfw5-1.xx&oh=c87c3905e336d7235594112d4b2d3db0&oe=5D91B150' 
   },
   {
     name: 'Kelly',
     userName: 'KellyT',
-    profilePic: 'https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-0/p110x80/48359292_220566528875309_249279966284349440_n.jpg?_nc_cat=104&_nc_ht=scontent-dfw5-1.xx&oh=c87c3905e336d7235594112d4b2d3db0&oe=5D91B150' 
+    email: 'kelly@gmail.com',
+    profilePicURL: 'https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-0/p110x80/48359292_220566528875309_249279966284349440_n.jpg?_nc_cat=104&_nc_ht=scontent-dfw5-1.xx&oh=c87c3905e336d7235594112d4b2d3db0&oe=5D91B150' 
   },
   {
     name: 'Ezra',
     userName: 'EzraMoges',
-    profilePic: 'https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-0/p110x80/48359292_220566528875309_249279966284349440_n.jpg?_nc_cat=104&_nc_ht=scontent-dfw5-1.xx&oh=c87c3905e336d7235594112d4b2d3db0&oe=5D91B150' 
+    email: 'ezuyea@gmail.com',
+    profilePicURL: 'https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-0/p110x80/48359292_220566528875309_249279966284349440_n.jpg?_nc_cat=104&_nc_ht=scontent-dfw5-1.xx&oh=c87c3905e336d7235594112d4b2d3db0&oe=5D91B150' 
   }
 ], {
   validate: true,
