d1e601acfd8f2976604a1c85c8bc8eb9466cba94
Chris Corley <chris.corley05@gmail.com>
2019-05-30 17:50:20 -0500

    Add message POST request handler

--- server/routes/message-route.js
+++ server/routes/message-route.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const helpers = require('../../database-mysql/db-helpers');
 
 const router = express.Router();
 
-router.get('/', function(req, res) {
-    res.send('GET handler for /api/message route.');
+router.get('/', function (req, res) {
+    // res.send('GET handler for /api/message route.');
+
 });
 
-router.post('/', function(req, res) {
-    res.send('POST handler for /api/message route.');
+router.post('/', function (req, res) {
+    // res.send('POST handler for /api/message route.');
+    helpers.saveMessage()
+        .then((message) => {
+            res.send(message);
+        }).catch((error) => {
+            res.send(error);
+        })
 });
 
 module.exports = router;
