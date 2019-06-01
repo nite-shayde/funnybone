50cca1f5f146ae1f9d895a76338129f1046b1eef
Chris Corley <chris.corley05@gmail.com>
2019-06-01 14:33:56 -0500

    Addeed user-route, re-committing server/index

--- server/index.js
+++ server/index.js
@@ -13,8 +13,8 @@ const morgan = require('morgan')
 
 
 const app = express();
-
 // MIDDLEWARE
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParse());
