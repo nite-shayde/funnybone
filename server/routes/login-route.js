3b9e3a27c4aeb0b3ce15fccad3e27a43bdf519e3
Jesse Lessinger <jesse.lessinger@gmail.com>
2019-06-01 12:04:22 -0500

    Authorization for homepage

---
+++ server/routes/login-route.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const helpers = require('../../database-mysql/db-helpers');
+const router = express.Router();
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    Users.findOne({ where: { username: username } })
+    .then(user => {
+      if (!user) { 
+        return done(null, false);
+      }
+      if (user.password !== password) {
+         return done(null, false); 
+      }
+      return done(null, user);
+    }).catch((err) => {
+      return done(err);
+    })
+  }
+));
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+ 
+passport.deserializeUser(function(id, done) {
+  Users.findOne({ where: { id }}).then((user) => {
+    done(null, user);
+  }).catch(err => done(err, null));
+ 
+
+});
+
+
+router.get('/', (req, res) => {
+  res.send("This is the login page");
+})
+
+router.post('/', 
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+
+module.exports = { loginRoute: router, passport };
